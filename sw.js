8a457002137518417905fee7efdf7df12c5e0a35
Almenning <z65hv7fpm8@privaterelay.appleid.com>
2026-08-22 18:58:45 +0200

    Legg til enkel offline-støtte

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='flyt-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
