ac11c8678c9c1b4f593039654b98382a27169ade
Almenning <z65hv7fpm8@privaterelay.appleid.com>
2026-09-26 22:57:30 +0200

    Forny cache for lik Belønning-header

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flyt-v87';
+const CACHE='flyt-v88';
 const NETWORK_TIMEOUT_MS=5000;
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.webmanifest','./vendor/supabase-2.116.0.js','./sync-merge.js','./sync.js'])).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})());});
